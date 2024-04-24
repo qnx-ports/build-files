d00efe3e67644fd052cf1bfc99872f9544c8ca50
Jianyan Li <jianyli@blackberry.com>
2024-04-24 09:06:16 -0400

    boost: add initial build files

---
+++ boost/README.md
@@ -0,0 +1,30 @@
+# Compile the port for QNX
+
+**NOTE**: QNX ports are only supported from a Linux host operating system
+
+Don't forget the source qnxsdp-env.sh in your QNX SDP.
+
+```bash
+# Clone the qnx-ports and boost repos
+git clone https://gitlab.com/qnx/libs/qnx-ports.git
+git clone https://gitlab.com/qnx/libs/boost.git
+
+cd boost
+git submodule update --init --recursive
+cd tools/build && git apply ../../../qnx-ports/boost/tools_qnx.patch && cd -
+cd ../
+
+# Build
+OSLIST=nto make -C qnx-ports/boost/ install QNX_PROJECT_ROOT="$(pwd)/boost" JLEVEL=$(nproc)
+
+./qnx-ports/boost/build_and_install_tests.sh
+```
+
+Currently, when numpy is installed on your host system, the build fails:
+
+```console
+/usr/local/lib/python3.8/dist-packages/numpy/core/include/numpy/npy_endian.h:13:14: fatal error: endian.h: No such file or directory
+   13 |     #include <endian.h>
+```
+
+The workaround is to run `sudo pip3 uninstall numpy` to uninstall numpy
---
+++ boost/build_and_install_tests.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+if [ -z "$QNX_TARGET" ]; then
+    echo "Please source qnxsdp-env.sh script!"
+    exit
+fi
+
+# Copy test binaries to QNX_TARGET
+copy_test_binaries() {
+    test_folder_path="./qnx-ports/boost/$1/build/boost/bin.v2/boost_tests"
+    mkdir -p $test_folder_path
+
+    # Find test binaries and copy to $test_folder_path
+    for lib in "./qnx-ports/boost/$1/build/boost/bin.v2/libs"/* ; do
+        lib_name=$(basename $lib)
+        for test_folder in "$lib/test"/* ; do
+            for f in $(find $test_folder -type f);
+            do
+                if [[ -x "$f" ]]
+                then
+                    mkdir -p $test_folder_path/$lib_name
+                    cp $f $test_folder_path/$lib_name
+                fi
+            done
+        done
+    done
+
+    # Install binaries
+    if [[ $1 == "nto-aarch64-le" ]]; then
+        cp -rf $test_folder_path "$QNX_TARGET/aarch64le/usr/bin"
+
+        echo "Installing tests in $QNX_TARGET/aarch64le/usr/bin" 
+    fi
+
+    if [[ $1 == "nto-x86_64-o" ]]; then
+        cp -rf $test_folder_path "$QNX_TARGET/x86_64/usr/bin"
+
+        echo "Installing tests in $QNX_TARGET/x86_64/usr/bin"
+    fi
+
+    mkdir -p $test_folder_path/libs
+
+    for f in $(find ./qnx-ports/boost/$1/build/boost/bin.v2/libs -type f -name *.so*);
+    do
+        cp -f $f $test_folder_path/libs
+    done
+}
+
+# Build all tests under libs
+echo "Start building tests for Boost"
+
+for lib in "./qnx-ports/boost/nto-x86_64-o/build/boost/bin.v2/libs"/* ; do
+    QNX_PROJECT_ROOT="$(pwd)/boost" JLEVEL=8 make -C qnx-ports/boost/ test."$(basename $lib)" -i
+done
+
+# Copy test binaries to QNX_TARGET
+copy_test_binaries "nto-aarch64-le"
+copy_test_binaries "nto-x86_64-o"
