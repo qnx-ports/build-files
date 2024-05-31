dc51ddee90e1ebb0d42b07be53913903e999be70
James Choi <chachoi@blackberry.com>
2024-05-31 14:50:51 -0400

    Add quotation marks around the id commands to prevent whitespace errors

--- docker-build-qnx-image.sh
+++ docker-build-qnx-image.sh
@@ -3,8 +3,8 @@
 QNX_SDP_VERSION=qnx800
 
 docker build -t $QNX_SDP_VERSION \
-  --build-arg USER_NAME=$(id --user --name) \
-  --build-arg GROUP_NAME=$(id --group --name) \
-  --build-arg USER_ID=$(id --user) \
-  --build-arg GROUP_ID=$(id --group) \
+  --build-arg USER_NAME="$(id --user --name)" \
+  --build-arg GROUP_NAME="$(id --group --name)" \
+  --build-arg USER_ID="$(id --user)" \
+  --build-arg GROUP_ID="$(id --group)" \
   --build-arg QNX_SDP_VERSION=$QNX_SDP_VERSION .
