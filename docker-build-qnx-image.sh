f131a5a40680a7211cf9e06d35fb73a413ed05b9
James Choi <chachoi@blackberry.com>
2024-06-05 10:32:52 -0400

    Docker: add a workaround for commands returning 2 things

--- docker-build-qnx-image.sh
+++ docker-build-qnx-image.sh
@@ -3,8 +3,8 @@
 QNX_SDP_VERSION=qnx800
 
 docker build -t $QNX_SDP_VERSION \
-  --build-arg USER_NAME="$(id --user --name)" \
-  --build-arg GROUP_NAME="$(id --group --name)" \
-  --build-arg USER_ID="$(id --user)" \
-  --build-arg GROUP_ID="$(id --group)" \
+  --build-arg USER_NAME="$(id --user --name | awk '{print $1;}')" \
+  --build-arg GROUP_NAME="$(id --group --name | awk '{print $1;}')" \
+  --build-arg USER_ID="$(id --user | awk '{print $1;}')" \
+  --build-arg GROUP_ID="$(id --group | awk '{print $1;}')" \
   --build-arg QNX_SDP_VERSION=$QNX_SDP_VERSION .
