239db5651bec812099c104e0d1b8609105c3129a
Felix Xing <fxing@qnx.com>
2025-01-24 14:40:20 -0500

    update build files to new porting style

---
+++ ports/icu/common.mk
@@ -0,0 +1,113 @@
+ifndef QCONFIG
+QCONFIG=qconfig.mk
+endif
+include $(QCONFIG)
+
+include $(MKFILES_ROOT)/qmacros.mk
+
+NAME=icu
+
+ICU_VERSION = 76.1
+
+QNX_PROJECT_ROOT ?= $(PRODUCT_ROOT)/../../
+
+#$(INSTALL_ROOT_$(OS)) is pointing to $QNX_TARGET
+#by default, unless it was manually re-routed to
+#a staging area by setting both INSTALL_ROOT_nto
+#and USE_INSTALL_ROOT
+INSTALL_ROOT ?= $(INSTALL_ROOT_$(OS))
+ICU_INSTALL_DIR ?= $(INSTALL_ROOT)
+TEST_INSTALL_DIR=$(ICU_INSTALL_DIR)/$(CPUVARDIR)/$(PREFIX)/bin/icu_tests
+
+#A prefix path to use **on the target**. This is
+#different from INSTALL_ROOT, which refers to a
+#installation destination **on the host machine**.
+#This prefix path may be exposed to the source code,
+#the linker, or package discovery config files (.pc,
+#CMake config modules, etc.). Default is /usr/local
+PREFIX ?= /usr/local
+
+#choose Release or Debug
+MAKE_BUILD_TYPE ?= Release
+MAKE_BUILD_TEST ?= false
+
+
+#override 'all' target to bypass the default QNX build system
+ALL_DEPENDENCIES = $(NAME)_all
+.PHONY: $(NAME)_all install check clean
+
+CFLAGS += $(FLAGS) -D_QNX_SOURCE
+LDFLAGS += -Wl,--build-id=md5 -Wl,--allow-shlib-undefined
+
+include $(MKFILES_ROOT)/qtargets.mk
+
+#Set config flags
+CFLAGS += -fPIC
+CXXFLAGS += $(CFLAGS) -std=gnu++17
+
+#Default toolchain for linux (required by icu)
+CROSS_CC = 
+CROSS_CXX = 
+CROSS_AR = 
+CROSS_RANDLIB =
+CONFIGURE_CMD = $(QNX_PROJECT_ROOT)/icu4c/source/runConfigureICU Linux 
+CONFIGURE_ARGS =
+CONFIGURE_ENVS = CXXFLAGS=-std=gnu++17
+
+# config tasks
+TEST_BUILD_CMD = echo "No test will be built"
+TEST_INSTALL_CMD = echo "No test will be installed"
+BIN_INSTALL_CMD = echo "No binary will be installed"
+
+ifeq ($(OS), nto)
+    #Config toolchain for qnx
+    CROSS_CC = ${QNX_HOST}/usr/bin/qcc -Vgcc_nto$(CPUVARDIR)
+    CROSS_CXX = ${QNX_HOST}/usr/bin/q++ -Vgcc_nto$(CPUVARDIR)
+    CROSS_AR = ${QNX_HOST}/usr/bin/nto$(CPU)-ar
+    CROSS_RANDLIB = ${QNX_HOST}/usr/bin/nto$(CPU)-ranlib
+
+    CONFIGURE_CMD = $(QNX_PROJECT_ROOT)/icu4c/source/configure
+    CONFIGURE_ARGS = --host=$(CPU)-*-nto \
+                     --prefix=$(ICU_INSTALL_DIR)/$(PREFIX) \
+                     --exec-prefix=$(ICU_INSTALL_DIR)/$(CPUVARDIR)/$(PREFIX) \
+                     --with-cross-build=$(PWD)/../linux-x86_64-o/build \
+                     --with-data-packaging=auto \
+                     --srcdir=$(QNX_PROJECT_ROOT)/icu4c/source
+    CONFIGURE_ENVS = CFLAGS="$(CFLAGS)" \
+                     CXXFLAGS="$(CXXFLAGS)" \
+                     LDFLAGS="$(LDFLAGS)" \
+                     CC="$(CROSS_CC)" \
+                     CXX="$(CROSS_CXX)" \
+                     AR="$(CROSS_AR)" \
+                     RANDLIB="$(CROSS_RANDLIB)" \
+
+    BIN_INSTALL_CMD = make VERBOSE=1 install $(MAKE_ARGS)
+    ifeq ($(MAKE_BUILD_TEST), true)
+        TEST_BUILD_CMD = make VERBOSE=1 tests $(MAKE_ARGS)
+        TEST_INSTALL_CMD = TEST_INSTALL_DIR=$(TEST_INSTALL_DIR) \
+                           QNX_PROJECT_ROOT=$(QNX_PROJECT_ROOT) \
+                           sh ../../install_test.sh
+    endif
+endif
+
+
+MAKE_ARGS ?= -j $(firstword $(JLEVEL) 1)
+
+ifndef NO_TARGET_OVERRIDE
+$(NAME)_all:
+	@mkdir -p build
+	@cd build && $(CONFIGURE_CMD) $(CONFIGURE_ENVS) $(CONFIGURE_ARGS)
+	@cd build && make VERBOSE=1 all $(MAKE_ARGS)
+	@cd build && $(TEST_BUILD_CMD)
+
+install check: $(NAME)_all
+	@echo Installing...
+	@cd build && $(BIN_INSTALL_CMD)
+	@cd build && $(TEST_INSTALL_CMD)
+	@echo Done.
+
+clean iclean spotless:
+	rm -rf build
+
+uninstall:
+endif
