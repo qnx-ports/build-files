09c3c88f7e01618784e8feaec002f8aa7fb3bb8b
Felix Xing <fxing@qnx.com>
2025-02-14 15:57:30 -0500

    fix place holder

--- ports/pango/install_all.sh
+++ ports/pango/install_all.sh
@@ -17,7 +17,7 @@ wget https://ftp.gnu.org/pub/gnu/gettext/gettext-0.23.1.tar.gz && tar -xf gettex
 wget https://github.com/tlwg/libthai/releases/download/v0.1.29/libthai-0.1.29.tar.xz && tar -xf libthai-0.1.29.tar.xz
 
 for NN in $(seq 0 ${DEP_COUNT}); do
-    if $(${DEP_CLONE_CMD[$NN]}); then
+    if ${DEP_CLONE_CMD[$NN]}; then
         chmod +x ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh
         ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh
         QNX_PROJECT_ROOT="$(pwd)/${DEP_NAME_SRC[$NN]}" make -C "build-files/ports/${DEP_NAME[$NN]}/" install
