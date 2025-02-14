7e1a7958775c4ce1d85a7e698228599fbcc9eba1
Felix Xing <fxing@qnx.com>
2025-02-14 15:31:59 -0500

    ?

--- ports/pango/install_all.sh
+++ ports/pango/install_all.sh
@@ -13,7 +13,7 @@ DEP_COUNT=${#DEP_NAME[@]}
 DEP_COUNT=$(( DEP_COUNT - 1 ))
 
 for NN in $(seq 0 ${DEP_COUNT}); do
-    if "${DEP_CLONE_CMD[$NN]}"; then
+    if ${DEP_CLONE_CMD[$NN]}; then
         chmod +x ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh
         ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh
         QNX_PROJECT_ROOT="$(pwd)/${DEP_NAME_SRC[$NN]}" make -C "build-files/ports/${DEP_NAME[$NN]}/" install
