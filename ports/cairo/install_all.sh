d451b71f44afc960ca676e13709c7594fbb4f63a
Felix Xing <fxing@qnx.com>
2025-02-14 15:20:13 -0500

    fix permission

--- ports/cairo/install_all.sh
+++ ports/cairo/install_all.sh
@@ -10,6 +10,7 @@ DEP_COUNT=$(( DEP_COUNT - 1 ))
 
 for NN in $(seq 0 ${DEP_COUNT}); do
     if ${DEP_CLONE_CMD[$NN]}; then
+        chmod +x ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh
         ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh
         QNX_PROJECT_ROOT="$(pwd)/${DEP_NAME_SRC[$NN]}" make -C "build-files/ports/${DEP_NAME[$NN]}/" install
     fi
