0937e3629f6fa3b09a78f60f264624b62d07046e
Felix Xing <fxing@qnx.com>
2025-02-18 12:48:04 -0500

    fix harfbuzz dependencies

--- ports/harfbuzz/install_all.sh
+++ ports/harfbuzz/install_all.sh
@@ -1,13 +1,10 @@
-DEP_NAME=(graphite gettext-runtime icu glib freetype2 cairo)
-DEP_NAME_SRC=(graphite gettext-0.23.1 icu glib freetype cairo)
+DEP_NAME=(graphite icu glib freetype2 cairo)
+DEP_NAME_SRC=(graphite icu glib freetype cairo)
 DEP_CLONE_CMD=("git clone -b 1.3.14 https://github.com/silnrsi/graphite.git"
-               "echo"
                "git clone -b release-76-1 https://github.com/unicode-org/icu.git"
                "git clone https://gitlab.gnome.org/GNOME/glib.git"
                "git clone -b VER-2-13-3 https://gitlab.freedesktop.org/freetype/freetype.git"
                "git clone -b 1.18.2 https://gitlab.freedesktop.org/cairo/cairo.git")
-
-wget https://ftp.gnu.org/pub/gnu/gettext/gettext-0.23.1.tar.gz && tar -xf gettext-0.23.1.tar.gz
 
 DEP_COUNT=${#DEP_NAME[@]}
 DEP_COUNT=$(( DEP_COUNT - 1 ))
