194c686b6bdaf3cbbb2bdded5665b1192019d88f
Felix X. <fxing@qnx.com>
2025-02-25 20:43:53 +0000

    libpng: new, in support of freetype, dep of pango (#91)
    ...
    Update to existing QSC package

---
+++ ports/libpng/install_all.sh
@@ -0,0 +1,16 @@
+DEP_NAME=(zlib)
+DEP_NAME_SRC=(zlib)
+DEP_CLONE_CMD=("git clone -b v1.3.1 https://github.com/madler/zlib.git")
+
+DEP_COUNT=${#DEP_NAME[@]}
+DEP_COUNT=$(( DEP_COUNT - 1 ))
+
+for NN in $(seq 0 ${DEP_COUNT}); do
+    if ${DEP_CLONE_CMD[$NN]}; then
+        chmod +x ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh
+        ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh
+        if ! QNX_PROJECT_ROOT="$(pwd)/${DEP_NAME_SRC[$NN]}" make -C "build-files/ports/${DEP_NAME[$NN]}/" install; then
+            exit 1; 
+        fi
+    fi
+done
