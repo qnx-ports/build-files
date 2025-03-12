789ca3e40f4b539bd4a8e35db8612311f8cba209
Deep Chordia <dchordia@blackberry.com>
2025-03-12 19:15:35 +0530

    add NO_TARGET_OVERRIDE check for consistency

--- ports/libev/common.mk
+++ ports/libev/common.mk
@@ -5,5 +5,7 @@ include $(QCONFIG)
 
 include $(MKFILES_ROOT)/qmake-cfg.mk
 
+ifndef NO_TARGET_OVERRIDE
 clean:
-	ls -A | grep -v "GNUmakefile" | xargs -n 1 rm -rf
+	@ls -A | grep -v "GNUmakefile" | xargs -n 1 rm -rf
+endif
