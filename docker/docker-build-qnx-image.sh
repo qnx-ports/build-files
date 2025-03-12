495a4c7c2bc775050d252264e29e2484b4d04a98
nnarayanamurthy <nnarayanamurthy@blackberry.com>
2025-03-12 16:36:46 +0530

    Adding support for MAC and arm mechine

--- docker/docker-build-qnx-image.sh
+++ docker/docker-build-qnx-image.sh
@@ -2,9 +2,9 @@
 
 QNX_SDP_VERSION=qnx800
 
-docker build -t $QNX_SDP_VERSION \
-  --build-arg USER_NAME="$(id --user --name | awk '{print $1;}')" \
-  --build-arg GROUP_NAME="$(id --group --name | awk '{print $1;}')" \
-  --build-arg USER_ID="$(id --user | awk '{print $1;}')" \
-  --build-arg GROUP_ID="$(id --group | awk '{print $1;}')" \
+docker build -t $QNX_SDP_VERSION --platform linux/amd64 \
+  --build-arg USER_NAME="$(id -u -n | awk '{print $1;}')" \
+  --build-arg GROUP_NAME="$(id -g -n | awk '{print $1;}')" \
+  --build-arg USER_ID="$(id -u | awk '{print $1;}')" \
+  --build-arg GROUP_ID="$(id -g | awk '{print $1;}')" \
   --build-arg QNX_SDP_VERSION=$QNX_SDP_VERSION .
--- docker/docker-create-container.sh
+++ docker/docker-create-container.sh
@@ -2,8 +2,13 @@
 
 QNX_SDP_VERSION=qnx800
 
-docker run -it \
+docker create -it --name=qnx-build \
+  --platform linux/amd64 \
   --net=host \
   --privileged \
-  -v $HOME:$HOME \
+  -v $HOME:/home/$USER/ \
   "$QNX_SDP_VERSION:latest" /bin/bash --rcfile /usr/local/qnx/.qnxbashrc
+
+docker start qnx-build
+
+docker exec -it qnx-build /bin/bash --rcfile /usr/local/qnx/.qnxbashrc
