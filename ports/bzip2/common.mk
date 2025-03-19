32e9e3f4d67681ff6eb889fe5469acef777e94af
Felix Xing <fxing@qnx.com>
2025-03-19 16:56:33 -0400

    fiz bzip2 do not respect execdir

--- ports/bzip2/common.mk
+++ ports/bzip2/common.mk
@@ -42,32 +42,30 @@ PKG_CONFIG_LIBDIR_IN = $($(NAME)_INSTALL_DIR)/lib/pkgconfig:$($(NAME)_INSTALL_DI
 PKG_CONFIG_TARGET_IN = $(QNX_TARGET)/$(CPUVARDIR)/$(PREFIX)/lib/pkgconfig:$(QNX_TARGET)/$(CPUVARDIR)/$(PREFIX)/share/pkgconfig
 export PKG_CONFIG_LIBDIR = $(PKG_CONFIG_LIBDIR_IN):$(PKG_CONFIG_TARGET_IN)
 
-PREFIX_ARGS = PREFIX=$(INSTALL_ROOT)/$(PREFIX)\
-			  EXEC_PREFIX=$($(NAME)_INSTALL_DIR)
-
 MAKE_ARGS ?= -j $(firstword $(JLEVEL) 1) -C $(QNX_PROJECT_ROOT) \
                                          CC="${QNX_HOST}/usr/bin/qcc -Vgcc_$(OS)$(CPUVARDIR)" \
                                          AR="${QNX_HOST}/usr/bin/$(OS)$(CPU)-ar" \
                                          RANLIB="${QNX_HOST}/usr/bin/$(OS)$(CPU)-ranlib" \
                                          CFLAGS="$(CFLAGS) -O3" \
-                                         LDFLAGS="$(LDFLAGS)" \
-										 $(PREFIX_ARGS)
+                                         LDFLAGS="$(LDFLAGS)"
 
 $(NAME)_all:
-	@make clean $(MAKE_ARGS) 
-	@make bzip2 bzip2recover libbz2.a $(MAKE_ARGS) 
-	@make -f Makefile-libbz2_so $(MAKE_ARGS)
+	@make clean $(MAKE_ARGS) PREFIX=$($(NAME)_INSTALL_DIR)
+	@make bzip2 bzip2recover libbz2.a $(MAKE_ARGS) PREFIX=$($(NAME)_INSTALL_DIR)
+	@make -f Makefile-libbz2_so $(MAKE_ARGS) PREFIX=$($(NAME)_INSTALL_DIR)
+	@mkdir -p $(INSTALL_ROOT)/$(PREFIX)/include
 
 install check: $(NAME)_all
 	@echo Installing...
-	@make install bzip2 bzip2recover libbz2.a $(MAKE_ARGS)
+	@make install bzip2 bzip2recover libbz2.a $(MAKE_ARGS) PREFIX=$($(NAME)_INSTALL_DIR)
 	@cd $(QNX_PROJECT_ROOT) && cp libbz2.so.1.0 $($(NAME)_INSTALL_DIR)/lib
 	@cd $(QNX_PROJECT_ROOT) && cp libbz2.so.1.0.8 $($(NAME)_INSTALL_DIR)/lib
+	@cp $($(NAME)_INSTALL_DIR)/include/bzlib.h $(INSTALL_ROOT)/$(PREFIX)/include
 	@echo Done.
 
 clean iclean spotless:
-	@make clean $(MAKE_ARGS)
-	@make clean -f Makefile-libbz2_so $(MAKE_ARGS)
+	@make clean $(MAKE_ARGS) PREFIX=$($(NAME)_INSTALL_DIR)
+	@make clean -f Makefile-libbz2_so $(MAKE_ARGS) PREFIX=$($(NAME)_INSTALL_DIR)
 	
 uninstall:
 
