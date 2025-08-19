563e48ab690b288d706dd5a15d0ca22e64188cdb
Felix Xing <fxing@qnx.com>
2025-08-19 16:36:47 -0400

    pango: update ci and install_all.sh

--- ports/pango/install_all.sh
+++ ports/pango/install_all.sh
@@ -8,17 +8,41 @@ DEP_CLONE_CMD=("git clone https://github.com/qnx-ports/fribidi.git"
                "git clone -b 1.18.2 https://gitlab.freedesktop.org/cairo/cairo.git"
                "git clone -b 10.2.0 https://github.com/harfbuzz/harfbuzz.git")
 
+
+
+if ! test -f $(pwd)/libthai-0.1.29.tar.xz; then
+    wget https://github.com/tlwg/libthai/releases/download/v0.1.29/libthai-0.1.29.tar.xz && tar -xf libthai-0.1.29.tar.xz
+fi
+
+if ! test -f $(pwd)/libthai-0.1.29.tar.xz; then
+    echo "libthai-0.1.29.tar.xz is not fetched"
+    exit 1;
+fi
+
 DEP_COUNT=${#DEP_NAME[@]}
 DEP_COUNT=$(( DEP_COUNT - 1 ))
 
-wget https://github.com/tlwg/libthai/releases/download/v0.1.29/libthai-0.1.29.tar.xz && tar -xf libthai-0.1.29.tar.xz
-
 for NN in $(seq 0 ${DEP_COUNT}); do
-    if ${DEP_CLONE_CMD[$NN]}; then
-        chmod +x ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh
-        ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh
+    if ! ${DEP_CLONE_CMD[$NN]}; then
+        echo "A git command just fail, but this maybe expected."
+        echo "If the build process does not stop, you can ignore this error."
+    fi
+
+    if test -d "$(pwd)/${DEP_NAME_SRC[$NN]}"; then
+        if chmod +x ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh; then
+            if ! ./build-files/ports/"${DEP_NAME[$NN]}"/install_all.sh; then
+                echo "install_all.sh: A recursive install_all.sh does exist for ${DEP_NAME[$NN]}, but it fails"
+                exit 1; 
+            fi
+        fi
+
+
         if ! QNX_PROJECT_ROOT="$(pwd)/${DEP_NAME_SRC[$NN]}" make -C "build-files/ports/${DEP_NAME[$NN]}/" install; then
+            echo "install_all.sh: Building ${DEP_NAME_SRC[$NN]} fails."
             exit 1; 
         fi
+    else
+        echo "install_all.sh: The source codes of "${DEP_NAME[$NN]}" are not cloned, abort."
+        exit 1;
     fi
 done
