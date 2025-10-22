897a6ed285ecb62b00a3fea80d17897c99850f68
Pavlo Kleymonov <pkleymonov@blackberry.com>
2025-10-22 16:59:31 +0200

    conan: cmake build and tests setup

--- conan/tools/ctest.py
+++ conan/tools/ctest.py
@@ -5,121 +5,114 @@ Fix absolute paths in CTestTestfile.cmake before run ctest
 import re
 import os
 
-def GetPaths(lines):
-    source_path = ""
-    build_path = ""
-    source_ptrn = re.compile(r'Source\s+directory\s*:\s*(/[-/\w\.]+)', re.IGNORECASE)
-    build_ptrn = re.compile(r'Build\s+directory\s*:\s*(/[-/\w\.]+)', re.IGNORECASE)
-    for l in lines:
-        if 0 == len(source_path):
-            m = source_ptrn.search(l)
-            if None != m:
-                source_path = m.group(1)
-        if 0 == len(build_path):
-            m = build_ptrn.search(l)
-            if None != m:
-                build_path = m.group(1)
-        if len(source_path)>0 and len(build_path)>0:
-            break
-    return source_path, build_path
+"""
+replace all oldstr string to the newstr one in file
+"""
+def replaceinfile(oldstr, newstr, fileName):
+    try:
+        with open(fileName, 'r', encoding='utf-8') as f:
+            filedata = f.read()
+    except Exception:
+        print(f"WRONG file format: {fileName}")
+        return
 
-def GetPythonPath(lines):
+    filedata = filedata.replace(oldstr, newstr)
+
+    with open(fileName, 'w', encoding='utf-8') as f:
+        f.write(filedata)
+
+"""
+extract actual python path from file
+"""
+def getpythonpath(filename):
     python_path = ""
-    python_ptrn = re.compile(r'"(([\s]*python|.*/python)(\d+\.?\d*)?)"', re.IGNORECASE)
-    for l in lines:
-        if 0 == len(python_path):
+    python_ptrn = re.compile(r'(([\s]*python|/[\w/]*/python)(\d+\.?\d*)?)"', re.IGNORECASE)
+    with open(filename, 'r', encoding='utf-8') as f:
+        for l in f.readlines():
             m = python_ptrn.search(l)
-            if None != m:
+            if None is not m:
                 python_path = m.group(1)
                 break
     return python_path
 
-def commonsuffixpath(paths):
-
-    parts_list = [[i for i in path.split(os.sep) if 0<len(i)] for path in paths]
-
-    node = ""
-    index = -1
-    suffix = []
-    stop = False
-    while not stop:
-        for parts in parts_list:
-            if 0 >= len(parts)+index+1:
-                stop = True
+"""
+extract source and build paths from file
+"""
+def getpaths(filename):
+    source_path = ""
+    build_path = ""
+    source_ptrn = re.compile(r'Source\s+directory\s*:\s*(/[-/\w\.]+)', re.IGNORECASE)
+    build_ptrn = re.compile(r'Build\s+directory\s*:\s*(/[-/\w\.]+)', re.IGNORECASE)
+    with open(filename, 'r', encoding='utf-8') as f:
+        for l in f.readlines():
+            if 0 == len(source_path):
+                m = source_ptrn.search(l)
+                if None is not m:
+                    source_path = m.group(1)
+            if 0 == len(build_path):
+                m = build_ptrn.search(l)
+                if None is not m:
+                    build_path = m.group(1)
+            if len(source_path)>0 and len(build_path)>0:
                 break
-            if 0 == len(node):
-                node = parts[index]
-            else:
-                if node != parts[index]:
-                    stop = True
-                    break
-                else:
-                    continue
-        if not stop:
-            if 0 < len(node):
-                suffix.append(node)
-                node = ""
-                index -= 1
-            else:
-                stop = True
-    suffix.reverse()
-    return os.sep.join(suffix)
+    return source_path, build_path
+
+"""
+return path to the top CTestTestfile.cmake
+"""
+def getrootCTestTestfile(root):
+    l = [os.path.join(r, "CTestTestfile.cmake") for r,d,f in os.walk(root) if "CTestTestfile.cmake" in f]
+    rootCTest = ""
+    for nextCTestFile in l:
+        if len(rootCTest) == 0:
+            rootCTest = nextCTestFile
+        elif len(rootCTest) > len(nextCTestFile):
+            rootCTest = nextCTestFile
+    return rootCTest
+
+"""
+build dict with pairs (pattern->list of paths)
+"""
+def getfilesmap(root, glob):
+    res = {}
+    for ent in glob:
+        res[ent] = [os.path.join(r, file) for r,d,f in os.walk(root) for file in f if file.endswith(ent)]
+    return res
+
+"""
+fix python path to the specified pythonname in file
+"""
+def fixpythonname(pythonname, files):
+    for f in files:
+        oldpythonname = getpythonpath(f)
+        if len(oldpythonname)>0:
+            replaceinfile(oldpythonname, pythonname, f)
+
+"""
+fix oldpath to newpath in file
+"""
+def fixpaths(oldpath, newpath, files):
+    for f in files:
+        replaceinfile(oldpath, newpath, f)
 
 if __name__ == "__main__":
+    currentpath = os.getcwd()
+    src, bld = getpaths(getrootCTestTestfile(currentpath))
+    commonpath = os.path.commonpath([src, bld])
+    glob = ["CTestTestfile.cmake", ".cmake", ".tcl", ".txt", ".yaml"]
+    filesmap = getfilesmap(currentpath, glob)
 
-    ROOT_FN = "CTestTestfile.cmake"
+    print(f"Fix python paths in CTestTestfile.cmake [{len(filesmap['CTestTestfile.cmake'])}]")
+    fixpythonname("python3", filesmap["CTestTestfile.cmake"])
 
-    ctestfilelist = [f"{root}/{ROOT_FN}" for root, dirs, files in os.walk('.') if f"{ROOT_FN}" in files ]
-    root_ctestfilelist = ""
-    for fn in ctestfilelist:
-        print(f"Find {fn} try to fix absolute paths...")
+    print(f"Fix src/build paths in *.cmake [{len(filesmap['.cmake'])}]")
+    fixpaths(commonpath, currentpath, filesmap[".cmake"])
 
-        fin = open(fn, mode="r")
-        all_lines = fin.readlines()
-        fin.close()
+    print(f"Fix src/build paths in *.tcl [{len(filesmap['.tcl'])}]")
+    fixpaths(commonpath, currentpath, filesmap[".tcl"])
 
-        root_ctestfilelist = os.path.dirname(os.path.abspath(fn))
+    print(f"Fix src/build paths in *.txt [{len(filesmap['.txt'])}]")
+    fixpaths(commonpath, currentpath, filesmap[".txt"])
 
-
-        src, bld = GetPaths(all_lines)
-        if 0 == len(src):
-            print("Error: cannot find source path")
-            continue
-        if 0 == len(bld):
-            print("Error: cannot find build path")
-            continue
-        pyp = GetPythonPath(all_lines)
-
-        com_suffix = commonsuffixpath([bld,root_ctestfilelist])
-        if 0 == len(com_suffix):
-            print("Error: cannot find common suffix between real and original build paths")
-            continue
-
-        com_part = os.path.commonpath([src, bld])
-        if 0 == len(com_part):
-            print("Error: cannot find common prefix between source and build paths")
-            continue
-
-        new_com_part = root_ctestfilelist[:root_ctestfilelist.rfind(com_suffix)].rstrip(os.sep)
-        if 0 == len(new_com_part):
-            print("Error: cannot detect new common prefix")
-            print("No needs to adjusting process")
-            continue
-
-        new_src = src.replace(com_part, new_com_part, 1)
-        new_bld = bld.replace(com_part, new_com_part, 1)
-        print(f"    Old Source path:{src}")
-        print(f"    Old Build  path:{bld}")
-        print(f"    New Source path:{new_src}")
-        print(f"    New Build  path:{new_bld}")
-
-        print(f"Backup original file {fn} to {fn+'.bak'}")
-        os.rename(fn, fn+".bak")
-        print("Fix all pathes...")
-        fout = open(fn, mode="w")
-        for l in all_lines:
-            l = l.replace(com_part,new_com_part)
-            if 0 < len(pyp):
-                l = l.replace(pyp,"python")
-            fout.write(l)
-        fout.close()
+    print(f"Fix src/build paths in *.yaml [{len(filesmap['.yaml'])}]")
+    fixpaths(commonpath, currentpath, filesmap[".yaml"])
