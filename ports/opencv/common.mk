6bc79ac712f179a8b85c49fa2509b8aa35c316ad
Ethan Leir <eleir@blackberry.com>
2025-11-11 15:02:24 -0500

    Fix syntax error.

--- ports/opencv/common.mk
+++ ports/opencv/common.mk
@@ -99,8 +99,8 @@ opencv_all:
 	@cd build && cmake $(CMAKE_ARGS) $(QNX_PROJECT_ROOT)
 	@cd build && make VERBOSE=1 all $(MAKE_ARGS)
 	if [ "$(BUILD_TESTING)" = "ON" ]; then \
-		cp -r $(EXTRA_ROOT)/testdata ./testdata \
-		cp -r $(QNX_PROJECT_ROOT)/samples/data ./sampledata \
+		cp -r $(EXTRA_ROOT)/testdata ./testdata; \
+		cp -r $(QNX_PROJECT_ROOT)/samples/data ./sampledata; \
 	fi
 
 install check: opencv_all
