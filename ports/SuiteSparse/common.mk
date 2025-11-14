b7999b884594fc38b4e640bab23dbdfc4dc72567
hyarasi13 <hyarasi@qnx.com>
2025-11-14 17:22:11 +0530

    Adding SuiteSparse_build-files (#238)

---
+++ ports/SuiteSparse/common.mk
@@ -0,0 +1,110 @@
+ifndef QCONFIG
+QCONFIG=qconfig.mk
+endif
+include $(QCONFIG)
+
+include $(MKFILES_ROOT)/qmacros.mk
+
+NAME = SuiteSparse
+
+QNX_PROJECT_ROOT ?= $(PRODUCT_ROOT)/../../$(NAME)
+
+#$(INSTALL_ROOT_$(OS)) is pointing to $QNX_TARGET
+#by default, unless it was manually re-routed to
+#a staging area by setting both INSTALL_ROOT_nto
+#and USE_INSTALL_ROOT
+INSTALL_ROOT ?= $(INSTALL_ROOT_$(OS))
+
+#A prefix path to use **on the target**. This is
+#different from INSTALL_ROOT, which refers to a
+#installation destination **on the host machine**.
+#This prefix path may be exposed to the source code,
+#the linker, or package discovery config files (.pc,
+#CMake config modules, etc.). Default is /usr/local
+PREFIX ?= usr/local
+
+#choose Release or Debug
+CMAKE_BUILD_TYPE ?= Release
+
+#override 'all' target to bypass the default QNX build system
+ALL_DEPENDENCIES = $(NAME)_all
+.PHONY: $(NAME)_all install check clean
+
+include $(MKFILES_ROOT)/qtargets.mk
+
+#Search paths for all of CMake's find_* functions --
+#headers, libraries, etc.
+#
+#$(QNX_TARGET): for architecture-agnostic files shipped with SDP (e.g. headers)
+#$(QNX_TARGET)/$(CPUVARDIR): for architecture-specific files in SDP
+#$(INSTALL_ROOT)/$(CPUVARDIR): any packages that may have been installed in the staging area
+CMAKE_FIND_ROOT_PATH := $(QNX_TARGET);$(QNX_TARGET)/$(CPUVARDIR);$(INSTALL_ROOT)/$(CPUVARDIR)
+
+#Path to CMake modules; These are CMake files installed by other packages
+#for downstreams to discover them automatically. We support discovering
+#CMake-based packages from inside SDP or in the staging area.
+#Note that CMake modules can automatically detect the prefix they are
+#installed in.
+CMAKE_MODULE_PATH := $(QNX_TARGET)/$(CPUVARDIR)/$(PREFIX)/lib/cmake;$(INSTALL_ROOT)/$(CPUVARDIR)/$(PREFIX)/lib/cmake
+
+#Headers from INSTALL_ROOT need to be made available by default
+#because CMake and pkg-config do not necessary add it automatically
+#if the include path is "default"
+CFLAGS += $(FLAGS) \
+          -I$(QNX_TARGET)/$(CPUVARDIR)/$(PREFIX)/include \
+          -I$(INSTALL_ROOT)/$(CPUVARDIR)/$(PREFIX)/include \
+          -I$(QNX_TARGET)/$(PREFIX)/include \
+          -D_QNX_SOURCE -fno-tree-vectorize
+
+LDFLAGS += -Wl,--build-id=md5
+
+WITH_STATIC_LIB ?= ON
+WITH_SHARED_LIB ?= ON
+WITH_TESTS ?= ON
+WITH_EXAMPLES ?= ON
+
+CMAKE_ARGS = -DCMAKE_TOOLCHAIN_FILE=$(PROJECT_ROOT)/qnx.nto.toolchain.cmake \
+             -DCMAKE_SYSTEM_PROCESSOR=$(CPU) \
+             -DCMAKE_C_FLAGS="$(CFLAGS)" \
+             -DCMAKE_EXE_LINKER_FLAGS="$(LDFLAGS)" \
+             -DCMAKE_CXX_COMPILER_TARGET=gcc_nto$(CPUVARDIR) \
+             -DCMAKE_C_COMPILER_TARGET=gcc_nto$(CPUVARDIR) \
+             -DCMAKE_INSTALL_PREFIX=$(INSTALL_ROOT) \
+             -DCMAKE_INSTALL_LIBDIR=$(CPUVARDIR)/$(PREFIX)/lib \
+             -DCMAKE_INSTALL_BINDIR=$(CPUVARDIR)/$(PREFIX)/bin \
+             -DCMAKE_INSTALL_INCLUDEDIR=$(CPUVARDIR)/$(PREFIX)/include \
+             -DCMAKE_MODULE_PATH="$(CMAKE_MODULE_PATH)" \
+             -DCMAKE_FIND_ROOT_PATH="$(CMAKE_FIND_ROOT_PATH)" \
+             -DCMAKE_BUILD_TYPE=$(CMAKE_BUILD_TYPE) \
+             -DWITH_STATIC_LIB=$(WITH_STATIC_LIB) \
+             -DWITH_SHARED_LIB=$(WITH_SHARED_LIB) \
+             -DWITH_TESTS=$(WITH_TESTS) \
+             -DWITH_EXAMPLES=$(WITH_EXAMPLES) \
+             -DBLAS_LIBRARIES=$(QNX_TARGET)/$(CPUVARDIR)/usr/local/lib \
+             -DBLAS_INCLUDE_DIR=$(QNX_TARGET)/$(CPUVARDIR)/usr/local/include \
+             -DCMAKE_PREFIX_PATH=$(QNX_TARGET)/$(CPUVARDIR)/usr/local/lib \
+             -DSUITESPARSE_USE_FORTRAN=OFF
+
+MAKE_ARGS ?= -j $(firstword $(JLEVEL) 1)
+
+ifndef NO_TARGET_OVERRIDE
+
+$(NAME)_all:
+	@export PKG_CONFIG_PATH=$(QNX_TARGET)/$(CPUVARDIR)/usr/local/lib/pkgconfig; \
+	mkdir -p build; \
+	cd build && cmake $(CMAKE_ARGS) $(QNX_PROJECT_ROOT); \
+	make VERBOSE=1 all $(MAKE_ARGS)
+
+install check: $(NAME)_all
+	@export PKG_CONFIG_PATH=$(QNX_TARGET)/$(CPUVARDIR)/usr/local/lib/pkgconfig; \
+	echo Installing...; \
+	cd build && make VERBOSE=1 install $(MAKE_ARGS); \
+	echo Done.
+
+clean iclean spotless:
+	rm -rf build
+
+uninstall:
+	rm -rf $(QNX_TARGET)/$(CPUVARDIR)/$(PREFIX)/lib/suitesparse*; \
+	rm -rf $(QNX_TARGET)/$(PREFIX)/include/suitesparse
+endif
