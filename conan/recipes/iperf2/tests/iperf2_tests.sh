4d62064f35ce8f573e627dc43b4d280e151e1a20
Pavlo Kleymonov <150929995+pkleymonov-qnx@users.noreply.github.com>
2026-03-11 12:54:22 +0100

    conan: iperf2 test_script update (#288)

--- conan/recipes/iperf2/tests/iperf2_tests.sh
+++ conan/recipes/iperf2/tests/iperf2_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-function print_test_name {
+print_test_name() {
     printf "##%${#1}s##\n" | tr ' ' '#'
     printf "# ${1} #\n"
     printf "##%${#1}s##\n" | tr ' ' '#'
 }
 
-function wait_for_exit {
+wait_for_exit() {
     server_pid=$1
     timeout=$2
     echo "waiting for exit server:$server_pid ..."
@@ -15,7 +15,7 @@ function wait_for_exit {
     sleep $timeout
 }
 
-function run_any {
+run_any() {
     name=$1
     srv_cmd=$2
     cln_cmd=$3
@@ -27,7 +27,7 @@ function run_any {
     wait_for_exit $srv_pid 1
 }
 
-function run_wait {
+run_wait() {
     run_any "${2}" "${3}" "${4}"
     echo "waiting more for ${1}sec."
     sleep $1
@@ -57,32 +57,27 @@ run_any "t14_udp_triptimes.sh port 5014"    "./iperf -p 5014 -s -P 1 -u -i 1 -t 
 run_any "t15_udp_enhanced.sh port 5015"     "./iperf -p 5015 -s -P 1 -u -i 1 -t 12 -e 2>&1" "./iperf -p 5015 -c 127.0.0.1 -P 1 -u -b 10m -i 1 -e"
 run_any "t16_udp_histograms.sh port 5016"   "./iperf -p 5016 -s -P 1 -u -i 1 -t 12 --histograms 2>&1" "./iperf -p 5016 -c 127.0.0.1 -P 1 -u -b 10m -i 1 --trip-times"
 
-if [ "QNX" = "$OS" ]; then
-    echo "Start QNX specific tests..."
-    run_wait 5 "low-water mark option is not applied"   "./iperf -s -i1 -t12 -l1M -e 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
-    run_wait 10 "low-water mark value is not specified" "./iperf -s -i1 -t12 -l1M -e --recvlowat 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
-    run_wait 10 "low-water mark value is 0"             "./iperf -s -i1 -t12 -l1M -e --recvlowat=0 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
-    run_wait 10 "low-water mark value is -1"            "./iperf -s -i1 -t12 -l1M -e --recvlowat=-1 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
-    run_wait 5 "low-water mark value is 1Kb"            "./iperf -s -i1 -t12 -l1M -e --recvlowat=1K 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
-    run_wait 5 "low-water mark value is 16Kb"           "./iperf -s -i1 -t12 -l1M -e --recvlowat=16K 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
-    run_wait 10 "low-water mark value is 32Kb"          "./iperf -s -i1 -t12 -l1M -e --recvlowat=32K 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
-    run_wait 10 "low-water mark value is 1Mb"           "./iperf -s -i1 -t12 -l1M -e --recvlowat=1M 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
-    run_wait 10 "low-water mark value is 5Mb"           "./iperf -s -i1 -t12 -l10M -e --recvlowat=5M 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
-    run_any "recv-mmsg/send-mmsg default/default 1Gbits" "./iperf -u -s -i1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b1G"
-    run_any "recv-mmsg/send-mmsg default/1 1Gbits"      "./iperf -u -s -i1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b1G --send-mmsg=1"
-    run_any "recv-mmsg/send-mmsg 1/default 1Gbits"      "./iperf -u -s -i1 --recv-mmsg=1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b1G"
-    run_any "recv-mmsg/send-mmsg 1/1 1Gbits"            "./iperf -u -s -i1 --recv-mmsg=1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 1G --send-mmsg=1"
-    run_any "recv-mmsg/send-mmsg 5/5 1Gbits"            "./iperf -u -s -i1 --recv-mmsg=5 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 1G --send-mmsg=5"
-    run_any "recv-mmsg/send-mmsg 10/10 1Gbits"          "./iperf -u -s -i1 --recv-mmsg=10 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 1G --send-mmsg=10"
-    run_any "recv-mmsg/send-mmsg 20/20 1Gbits"          "./iperf -u -s -i1 --recv-mmsg=20 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 1G --send-mmsg=20"
-    run_any "recv-mmsg/send-mmsg 1/1 5Gbits"            "./iperf -u -s -i1 --recv-mmsg=1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 5G --send-mmsg=1"
-    run_any "recv-mmsg/send-mmsg 5/5 5Gbits"            "./iperf -u -s -i1 --recv-mmsg=5 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 5G --send-mmsg=5"
-    run_any "recv-mmsg/send-mmsg 10/10 5Gbits"          "./iperf -u -s -i1 --recv-mmsg=10 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 5G --send-mmsg=10"
-    run_any "recv-mmsg/send-mmsg 20/20 5Gbits"          "./iperf -u -s -i1 --recv-mmsg=20 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 5G --send-mmsg=20"
-    run_any "recv-mmsg/send-mmsg 1/1 10Gbits"           "./iperf -u -s -i1 --recv-mmsg=1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b10G --send-mmsg=1"
-    run_any "recv-mmsg/send-mmsg 5/5 10Gbits"           "./iperf -u -s -i1 --recv-mmsg=5 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b10G --send-mmsg=5"
-    run_any "recv-mmsg/send-mmsg 10/10 10Gbits"         "./iperf -u -s -i1 --recv-mmsg=10 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b10G --send-mmsg=10"
-    run_any "recv-mmsg/send-mmsg 20/20 10Gbits"         "./iperf -u -s -i1 --recv-mmsg=20 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b10G --send-mmsg=20"
-else
-    echo "skip QNX specific tests for $OS"
-fi
+run_wait 5 "low-water mark option is not applied"   "./iperf -s -i1 -t12 -l1M -e 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
+run_wait 10 "low-water mark value is not specified" "./iperf -s -i1 -t12 -l1M -e --recvlowat 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
+run_wait 10 "low-water mark value is 0"             "./iperf -s -i1 -t12 -l1M -e --recvlowat=0 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
+run_wait 10 "low-water mark value is -1"            "./iperf -s -i1 -t12 -l1M -e --recvlowat=-1 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
+run_wait 5 "low-water mark value is 1Kb"            "./iperf -s -i1 -t12 -l1M -e --recvlowat=1K 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
+run_wait 5 "low-water mark value is 16Kb"           "./iperf -s -i1 -t12 -l1M -e --recvlowat=16K 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
+run_wait 10 "low-water mark value is 32Kb"          "./iperf -s -i1 -t12 -l1M -e --recvlowat=32K 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
+run_wait 10 "low-water mark value is 1Mb"           "./iperf -s -i1 -t12 -l1M -e --recvlowat=1M 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
+run_wait 10 "low-water mark value is 5Mb"           "./iperf -s -i1 -t12 -l10M -e --recvlowat=5M 2>&1" "./iperf -c 127.0.0.1 -i1 -e"
+run_any "recv-mmsg/send-mmsg default/default 1Gbits" "./iperf -u -s -i1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b1G"
+run_any "recv-mmsg/send-mmsg default/1 1Gbits"      "./iperf -u -s -i1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b1G --send-mmsg=1"
+run_any "recv-mmsg/send-mmsg 1/default 1Gbits"      "./iperf -u -s -i1 --recv-mmsg=1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b1G"
+run_any "recv-mmsg/send-mmsg 1/1 1Gbits"            "./iperf -u -s -i1 --recv-mmsg=1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 1G --send-mmsg=1"
+run_any "recv-mmsg/send-mmsg 5/5 1Gbits"            "./iperf -u -s -i1 --recv-mmsg=5 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 1G --send-mmsg=5"
+run_any "recv-mmsg/send-mmsg 10/10 1Gbits"          "./iperf -u -s -i1 --recv-mmsg=10 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 1G --send-mmsg=10"
+run_any "recv-mmsg/send-mmsg 20/20 1Gbits"          "./iperf -u -s -i1 --recv-mmsg=20 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 1G --send-mmsg=20"
+run_any "recv-mmsg/send-mmsg 1/1 5Gbits"            "./iperf -u -s -i1 --recv-mmsg=1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 5G --send-mmsg=1"
+run_any "recv-mmsg/send-mmsg 5/5 5Gbits"            "./iperf -u -s -i1 --recv-mmsg=5 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 5G --send-mmsg=5"
+run_any "recv-mmsg/send-mmsg 10/10 5Gbits"          "./iperf -u -s -i1 --recv-mmsg=10 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 5G --send-mmsg=10"
+run_any "recv-mmsg/send-mmsg 20/20 5Gbits"          "./iperf -u -s -i1 --recv-mmsg=20 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b 5G --send-mmsg=20"
+run_any "recv-mmsg/send-mmsg 1/1 10Gbits"           "./iperf -u -s -i1 --recv-mmsg=1 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b10G --send-mmsg=1"
+run_any "recv-mmsg/send-mmsg 5/5 10Gbits"           "./iperf -u -s -i1 --recv-mmsg=5 -t 12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b10G --send-mmsg=5"
+run_any "recv-mmsg/send-mmsg 10/10 10Gbits"         "./iperf -u -s -i1 --recv-mmsg=10 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b10G --send-mmsg=10"
+run_any "recv-mmsg/send-mmsg 20/20 10Gbits"         "./iperf -u -s -i1 --recv-mmsg=20 -t12 2>&1" "./iperf -u -c 127.0.0.1 -i1 -b10G --send-mmsg=20"
